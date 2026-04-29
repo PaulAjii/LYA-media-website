5385fd7e731d10cffe8c2cc81f83c7b87b514800
PaulAjii <paulajii134@gmail.com>
2026-04-29 23:34:41 +0100

    feat: add AlbumWithTracksDTO and TracksList types for album-tracks relationship

--- internal/albums/dtos/albums_dto.go
+++ internal/albums/dtos/albums_dto.go
@@ -1,10 +1,23 @@
 package dtos
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 
+	"github.com/PaulAjii/LYA-media-website/internal/tracks/dtos"
 	"github.com/google/uuid"
 )
+
+type TracksList []dtos.TrackDTO
+
+func (t *TracksList) Scan(src any) error {
+	bytes, ok := src.([]byte)
+	if !ok {
+		return fmt.Errorf("expected []byte for tracks, got %T", src)
+	}
+	return json.Unmarshal(bytes, t)
+}
 
 type Album struct {
 	ID           uuid.UUID `json:"id" db:"id"`
@@ -15,9 +28,24 @@ type Album struct {
 	CreatedAt    time.Time `json:"createdAt" db:"created_at"`
 }
 
+type AlbumWithTracksDTO struct {
+	Album  `json:"album"`
+	Tracks []dtos.TrackDTO `json:"tracks"`
+}
+
 type CreateAlbum struct {
 	Title        string    `json:"title"`
 	Date         time.Time `json:"date"`
 	Summary      *string   `json:"summary"`
 	ThumbnailURL *string   `json:"thumbnail_url"`
+}
+
+type AlbumWithTracks struct {
+	ID           uuid.UUID  `json:"id" db:"id"`
+	Title        string     `json:"title" db:"title"`
+	Date         time.Time  `json:"date" db:"date"`
+	Summary      *string    `json:"summary" db:"summary"`
+	ThumbnailURL *string    `json:"thumbnail_url" db:"thumbnail_url"`
+	CreatedAt    time.Time  `json:"createdAt" db:"created_at"`
+	Tracks       TracksList `json:"tracks"`
 }
