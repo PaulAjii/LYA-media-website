4402fdaee787862d85478bf448cd5fbc64039092
PaulAjii <paulajii134@gmail.com>
2026-04-29 16:18:46 +0100

    feat: create dtos for the track domain

---
+++ internal/tracks/dtos/tracks_dtos.go
@@ -0,0 +1,28 @@
+package dtos
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type TrackDTO struct {
+	ID          uuid.UUID `json:"id" db:"id"`
+	AlbumID     uuid.UUID `json:"albumId" db:"album_id"`
+	Title       string    `json:"title" db:"title"`
+	TrackNumber int       `json:"trackNumber" db:"track_number"`
+	AudioURL    string    `json:"audioUrl" db:"audio_url"`
+	CreatedAt   time.Time `json:"createdAt" db:"created_at"`
+}
+
+type CreateTrackDTO struct {
+	AlbumID     uuid.UUID `json:"albumId" validate:"required"`
+	Title       string    `json:"title" validate:"required"`
+	TrackNumber int       `json:"trackNumber" validate:"required"`
+	AudioURL    string    `json:"audioUrl" validate:"required,url"`
+}
+
+type UpdateTrackDTO struct {
+	Title       *string `json:"title,omitempty"`
+	TrackNumber *int    `json:"trackNumber,omitempty"`
+}
