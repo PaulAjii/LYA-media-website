1185b03caab45feed03a30c6d7221a31091d61a3
PaulAjii <paulajii134@gmail.com>
2026-04-29 16:19:20 +0100

    feat: add create track and get tracks methods to track domain

---
+++ internal/tracks/repository.go
@@ -0,0 +1,64 @@
+package tracks
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/PaulAjii/LYA-media-website/internal/tracks/dtos"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type TrackRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewTrackRepository(db *pgxpool.Pool) *TrackRepository {
+	return &TrackRepository{
+		db: db,
+	}
+}
+
+// Create Track
+func (r *TrackRepository) Create(ctx context.Context, payload dtos.CreateTrackDTO) (*dtos.TrackDTO, error) {
+	stmt := `
+		INSERT INTO tracks(album_id, title, track_number, audio_url)
+		VALUES($1, $2, $3, $4)
+		RETURNING id, album_id, title, track_number, audio_url, created_at
+	`
+
+	rows, err := r.db.Query(ctx, stmt, payload.AlbumID, payload.Title, payload.TrackNumber, payload.AudioURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute query: %w", err)
+	}
+	defer rows.Close()
+
+	track, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[dtos.TrackDTO])
+	if err != nil {
+		return nil, fmt.Errorf("failed to collect row: %w", err)
+	}
+
+	return &track, nil
+}
+
+func (r *TrackRepository) GetByAlbumID(ctx context.Context, albumID uuid.UUID) ([]dtos.TrackDTO, error) {
+	stmt := `
+		SELECT id, album_id, title, track_number, audio_url, created_at
+		FROM tracks
+		WHERE album_id = $1
+		ORDER BY track_number ASC
+	`
+	rows, err := r.db.Query(ctx, stmt, albumID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute query: %w", err)
+	}
+	defer rows.Close()
+
+	tracks, err := pgx.CollectRows(rows, pgx.RowToStructByName[dtos.TrackDTO])
+	if err != nil {
+		return nil, fmt.Errorf("failed to collect rows: %w", err)
+	}
+
+	return tracks, nil
+}
