e133a903694344a022d1866c9fdc150cd2d22a95
PaulAjii <paulajii134@gmail.com>
2026-04-29 15:24:33 +0100

    feat: update database connection setup with configuration parsing and options

--- pkg/db/postgres.go
+++ pkg/db/postgres.go
@@ -4,11 +4,17 @@ import (
 	"context"
 	"os"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func ConnectDB() (*pgxpool.Pool, error) {
-	pool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	config, _ := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
+
+	config.ConnConfig.StatementCacheCapacity = 0
+	config.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
 	if err != nil {
 		return nil, err
 	}
