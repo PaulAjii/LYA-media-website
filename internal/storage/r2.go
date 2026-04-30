7a16b0a51ccc7381351893d3c49472e4c42f7de9
PaulAjii <paulajii134@gmail.com>
2026-04-30 09:51:09 +0100

    feat: migrate from Supabase storage to R2 storage implementation

---
+++ internal/storage/r2.go
@@ -0,0 +1,86 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"mime/multipart"
+	"os"
+	"path/filepath"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type R2Storage struct {
+	client    *s3.Client
+	bucket    string
+	publicURL string
+}
+
+func NewR2Storage() *R2Storage {
+	bucketName := os.Getenv("R2_BUCKET_NAME")
+	accountID := os.Getenv("R2_ACCOUNT_ID")
+	accessKeyID := os.Getenv("R2_ACCESS_KEY_ID")
+	secretAccessKey := os.Getenv("R2_SECRET_ACCESS_KEY")
+
+	cfg, err := config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKeyID, secretAccessKey, "")),
+		config.WithRegion("auto"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.BaseEndpoint = aws.String(fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountID))
+	})
+
+	return &R2Storage{
+		client:    client,
+		bucket:    bucketName,
+		publicURL: os.Getenv("R2_PUBLIC_URL"),
+	}
+
+}
+
+func (s *R2Storage) UploadFile(ctx context.Context, file multipart.File, header *multipart.FileHeader, folder, trackTitle string) (string, error) {
+	ext := filepath.Ext(header.Filename)
+	key := fmt.Sprintf("%s/%s%s", folder, trackTitle, ext)
+
+	contentType := header.Header.Get("Content-Type")
+	if contentType == "" {
+		contentType = "application/octet-stream"
+	}
+
+	_, err := s.client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket:      aws.String(s.bucket),
+		Key:         aws.String(key),
+		Body:        file,
+		ContentType: aws.String(contentType),
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to upload file: %w", err)
+	}
+
+	publicURL := fmt.Sprintf("%s/%s", s.publicURL, key)
+	return publicURL, nil
+}
+
+func (s *R2Storage) Delete(ctx context.Context, publicURL string) error {
+	prefix := s.publicURL + "/"
+	key := publicURL[len(prefix):]
+
+	_, err := s.client.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(key),
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to delete file: %w", err)
+	}
+	return nil
+}
