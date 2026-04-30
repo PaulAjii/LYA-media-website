6f2c0c7f14f9a129b3de2aef2ae3898d7f0e96ea
PaulAjii <paulajii134@gmail.com>
2026-04-30 02:08:19 +0100

    feat: integrate Supabase storage into tracks handler for improved file management

--- internal/api/setup_routes.go
+++ internal/api/setup_routes.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"os"
+
 	"github.com/PaulAjii/LYA-media-website/internal/albums"
+	"github.com/PaulAjii/LYA-media-website/internal/storage"
 	"github.com/PaulAjii/LYA-media-website/internal/tracks"
 	"github.com/PaulAjii/LYA-media-website/pkg/db"
 	"github.com/gofiber/fiber/v3"
@@ -15,6 +18,11 @@ func SetupRoutes(app *fiber.App) {
 		panic(err)
 	}
 
+	supabaseStorage, err := storage.NewSupabaseStorage(os.Getenv("SUPABASE_URL"), os.Getenv("SUPABASE_SERVICE_ROLE_KEY"), os.Getenv("SUPABASE_BUCKET_NAME"))
+	if err != nil {
+		panic(err)
+	}
+
 	// albums route setup
 	albumsRepo := albums.NewRepository(pool)
 	albumsUsecase := albums.NewUseCase(albumsRepo)
@@ -24,6 +32,6 @@ func SetupRoutes(app *fiber.App) {
 	// tracks route setup
 	tracksRepo := tracks.NewTrackRepository(pool)
 	tracksUsecase := tracks.NewUseCase(tracksRepo)
-	tracksHandler := tracks.NewHandler(tracksUsecase)
+	tracksHandler := tracks.NewHandler(tracksUsecase, supabaseStorage)
 	tracks.SetupRoutes(api, tracksHandler)
 }
