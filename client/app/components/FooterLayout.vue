cbd3cb2956634be1ab5e9adaf8d9d9a6049de424
PaulAjii <paulajii134@gmail.com>
2026-05-04 19:02:22 +0100

    feat: add footer layout component with copyright information

---
+++ client/app/components/FooterLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <p class="bg-gray-100 px-6 py-4 text-sm text-gray-400 font-logo">
+        Copyright &copy; {{ date }}. Loyola Youth Assembly
+    </p>
+</template>
+
+<script setup lang="ts">
+const newDate = new Date()
+const date = newDate.getFullYear()
+
+</script>
