815dffed8c215b875b4517b726fd4f7ebdecc56d
PaulAjii <paulajii134@gmail.com>
2026-07-25 05:11:15 +0100

    feat: add sabilytics to main app

--- client/app/app.vue
+++ client/app/app.vue
@@ -4,3 +4,10 @@
     <AudioPlayer />
   </NuxtLayout>
 </template>
+
+<script
+  async
+  src="https://www.sabilytics.com/script.js"
+  data-site="t0bc2ojllsz2"
+  data-domain="lya-media-website.vercel.app"
+></script>
