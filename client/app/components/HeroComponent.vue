540bd53b8a5dff298ab7e23cb843e7dcb5d91b5a
PaulAjii <paulajii134@gmail.com>
2026-07-25 06:00:46 +0100

    feat: add h1 to homepage

--- client/app/components/HeroComponent.vue
+++ client/app/components/HeroComponent.vue
@@ -8,10 +8,10 @@
 
         <!-- Content -->
         <div class="absolute bottom-1/2 right-1/2 translate-1/2 px-8 py-8 flex items-center flex-col">
-            <span
+            <h1
                 class="inline-block text-[10px] tracking-widest uppercase text-center text-white bg-gray-500 rounded-full px-3 py-1 mb-3 font-logo sm:text-xs">
                 LOYOLA YOUTH ASSEMBLY
-            </span>
+            </h1>
             <p
                 class="w-md uppercase text-gray-300 font-black font-oswald text-5xl flex items-center justify-center flex-col gap-2 mt-2 sm:text:6xl sm:w-l md:w-3xl md:text-8xl">
                 <span class="text-blue-700">caring</span>
